219738caa59fabf5a729b2fbcdfa3a3a1f5bf8aa
JuanPablo <jupis442@gmail.com>
2023-08-29 07:35:10 -0500

    trabajo completado

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import { Router} from "express"
+const router = Router()
+
+router.get("/", (req,res) => res.render("index"))
+router.get("/services", (req,res) => res.render("services"))
+router.get("/about", (req,res) => res.render("about"))
+router.get("/contact", (req,res) => res.render("contact"))
+
+
+export default router
